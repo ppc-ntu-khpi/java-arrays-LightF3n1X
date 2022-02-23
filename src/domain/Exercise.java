bddd4fbcb2ab685eeac52702d6b8b0b5a0d038cb
F3n1X <54713542+LightF3n1X@users.noreply.github.com>
2022-02-23 14:38:25 +0200

    Update Exercise.java

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,27 @@
 package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static String Calculate(int n) {
+        boolean[] prime = new boolean[n + 1];
+        for (int i = 0; i <= n; i++) {
+            prime[i] = true;
+        }
+
+        for (int p = 2; p * p <= n; p++) {
+            if (prime[p]) {
+                for (int i = p * p; i <= n; i += p) {
+                    prime[i] = false;
+                }
+            }
+        }
+
+        StringBuilder primeNumbers = new StringBuilder();
+        for (int i = 2; i <= n; i++) {
+            if (prime[i]) {
+                primeNumbers.append(i).append(", ");
+            }
+        }
+
+        return primeNumbers.toString();
     }
 }
